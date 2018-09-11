7256b5a9a478a04f2f1011c3e5844abaa04ba671
Marcin Jerwan <m.jerwan@gmail.com>
2018-09-11 22:35:18 +0100

    extensions done

--- src/views/instrument_info.js
+++ src/views/instrument_info.js
@@ -18,6 +18,22 @@ InstrumentInfo.prototype.displayResults = function(wholeFamilyInfo){
     const paragraph = document.createElement('p');
     paragraph.textContent = wholeFamilyInfo.description;
     this.resultsSection.appendChild(paragraph);
+
+    const smallTitle = document.createElement('h3');
+    smallTitle.textContent = "Instruments from this family:"
+    this.resultsSection.appendChild(smallTitle);
+
+    const instrumentsUl = document.createElement('ul');
+    this.resultsSection.appendChild(instrumentsUl);
+
+ 
+
+
+    wholeFamilyInfo.instruments.forEach(element => {
+        const instrumentLi = document.createElement('li');
+        instrumentLi.textContent = element;
+        instrumentsUl.appendChild(instrumentLi);
+    });
 }
 
 
